0c1d3fa53952e53024f836550440b0f7ebfe5940
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-05-25 23:30:45 -0300

    feat: slider

---
+++ public/tests/pure-css-slider-with-arrows/js/index.js
@@ -0,0 +1,5 @@
+//Its completed. ( may be )
+//Watch me codeing this at: https://youtu.be/z74ExMz-cWU
+// A Image Slider by Mayur Birle
+
+//See Updated Version at  https://codepen.io/mayurbirle/details/NvXKPq/
